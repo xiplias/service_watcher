894b7f59e37a169f7990f0aca3c183765a2f0e34
Kasper Johansen <k@spernj.org>
2012-08-28 11:08:00 +0200

    Added mail plugin and memory usage plugin.

---
+++ plugins/class_servicewatcher_plugin_ssh_memory_usage.rb
@@ -0,0 +1,49 @@
+class Service_watcher::Plugin::Ssh_memory_usage < Service_watcher::Plugin
+	def self.paras
+		return [{
+			"title" => _("Hostname"),
+			"name" => "txthost"
+		},{
+			"title" => _("Port"),
+			"name" => "txtport",
+			"default" => "22"
+		},{
+			"title" => _("Username"),
+			"name" => "txtuser"
+		},{
+			"type" => "password",
+			"title" => _("Password"),
+			"name" => "txtpasswd"
+		},{
+			"title" => _("Warning percent"),
+			"name" => "txtwarnperc"
+		}]
+	end
+	
+	def self.check(paras)
+    warn_perc = paras["txtwarnperc"].to_f
+    
+    output = nil
+    Knj::Retry.try(:tries => 3, :timeout => 15, :wait => 2, :errors => [Errno::ETIMEDOUT, Errno::EHOSTUNREACH]) do
+      sshrobot = Knj::SSHRobot.new(
+        "host" => paras["txthost"],
+        "port" => paras["txtport"].to_i,
+        "user" => paras["txtuser"],
+        "passwd" => paras["txtpasswd"]
+      )
+      
+      if !(Float(paras["txtwarnperc"]) rescue false)
+        raise _("Warning percent is not numeric - please enter it correctly as number only.")
+      end
+      
+      output = sshrobot.exec("ps aux")
+      sshrobot.close
+    end
+    
+    Knj::Unix_proc.list("psaux_res" => output, "yield_data" => true) do |data|
+      if data["ram_last"].to_f >= warn_perc
+        raise "The process with PID '#{data["pid"]}' from '#{data["user"]}' had '#{data["ram_last"].to_f.round(2)}%' memory usage: '#{data["app"]}'."
+      end
+    end
+	end
+end
